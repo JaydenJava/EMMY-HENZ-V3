9c6925ce0f1a710fb0804f8de91ccac137700244
JaydenJava <jaydenkmb56@gmail.com>
2025-04-07 14:22:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkRwOGFNNzNmblBFYjB3ZmhVcy9jTU1KRm1SZmhqTUQ4OFR6NUh1UUtrbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTWYzVXhuQVJCRENDNWdpM1hOQVBGZzNLZVVCQnI5U3JldUR2WWhBNEtTcz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzT01MQnFwRDRkdGp6dUh1bGxWOGZiVWlranRSYlRmZXMxUVFRVmJ6NTFzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsd1V2QnErVzJ0cWNnclJXcjVnWUJmM1BrSE9ZUFZ0ajhJRE45ekI0KzNFPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InVPTis5M1pYb0VQTTlLcCs2elJQOXhtVWFyc3ZMYktrQmd2T2hodkc4V0E9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkQzVGRkMm5xQzNNTjh1dHpyRGsza1UzelY1d09OTEdibkFFenQ5WnVvaTg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMk1SZG1YQUpFbjdXL2RsSlFVTldNTTdiZjhnckhVT3FqYVVZY083aHpuUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib0gzZnBBYnNrYjJWblNWMWhsMS83dmx6ZVdFSVZVUDM1bkF0dG9mUnBIcz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ild5cW9CRzBwa2E1WVZBMk9CWmNtRTBnY3owWTFhOFE0ejRLdEl4UG9JQ2huTXhvYlU4K1JDd0M3VHRWNmlPT3Z2aXR3UkFxQ3JySUwwNmR2T0NQZUNBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTI2LCJhZHZTZWNyZXRLZXkiOiJHNVhFenBRc1c2S3ZHQTFtVmhjRXJkandpd3NHcXlRa2FxRG1ublRNZEc0PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiIxVHd"
 module.exports = {
   menu: process.env.MENU || "3",
   HANDLERS: process.env.PREFIX || ".",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "EMMY_HENZ-V3",
   packname: process.env.PACK_NAME || "E M M Y",
   botname: process.env.BOT_NAME || "EMMY_HENZ-V3",
-  ownername: process.env.OWNER_NAME || "Emmanuel Henshaw",
+  ownername: process.env.OWNER_NAME || "Jayden Java ",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
